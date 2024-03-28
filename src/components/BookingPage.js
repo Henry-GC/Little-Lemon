f05abdc2c737e808ac580c2371a3ad22268a369e
thecolchon <thecolchon@gmail.com>
2024-03-28 13:06:32 -0500

    api don´t work

--- src/components/BookingPage.js
+++ src/components/BookingPage.js
@@ -1,12 +1,18 @@
-import { useReducer, useState } from "react"
+import { useEffect, useReducer, useState } from "react"
 import BookingForm from "./BookingForm"
 
 const BookingPage = (props) => {
-    
+
+    const initializeTimes = () => {
+        const today = new Date();
+        const date = today.toISOString().split('T')[0];
+        return(
+                ["17:00","18:00","19:00","20:00","21:00","22:00"]
+            )}
     const updateTimes = (state,action) =>{
         return ["17:00","18:00","19:00","20:00","21:00","22:00"]
     }
-    const [availableTimes,dispatch] = useReducer(updateTimes,["17:00","18:00","19:00","20:00","21:00","22:00"])
+    const [availableTimes,dispatch] = useReducer(updateTimes,initializeTimes())
 
     return(
         <section className="reserve-container">
